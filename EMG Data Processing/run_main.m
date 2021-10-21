ef50a6e5e84610213df6c64f74cfc68820db64d4
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-10-21 16:16:15 -0400

    updated ip script for new board

--- EMG Data Processing/run_main.m
+++ EMG Data Processing/run_main.m
@@ -7,7 +7,10 @@ for muscle_number = 2:4
     run(process_data_script);
     run(filter_data_script);
     run(neuro_muscular_model_script);
-    combined_assistance = combined_assistance + assistance;
-    hold on;
+    combined_assistance = combined_assistance - assistance;
+    if muscle_number == 2
+        combined_assistance = combined_assistance + 2*assistance;
+    end
+    %hold on;
 end
 plot(time,combined_assistance); 
