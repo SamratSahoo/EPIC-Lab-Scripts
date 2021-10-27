b4093a3a6de3ba849cc733008588f652024bd3ef
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-10-27 16:44:07 -0400

    added threading

--- Display IP/display_ip.py
+++ Display IP/display_ip.py
@@ -1,32 +1,35 @@
 import socket
 import time
-import urllib
-
-# import SSD1306
+import SSD1306
 from PIL import Image, ImageDraw, ImageFont
+import threading
 
 
 def wait_for_internet_connection():
+    iteration = 0
     while True:
         try:
-            socket.create_connection(("www.google.com", 80))
-            return
+            iteration += 1
+            print(iteration)
+            if iteration == 10000:
+                break
+                socket.create_connection(("www.google.com", 80))
+                return
         except Exception as e:
             pass
 
 
 def get_ip():
-    wait_for_internet_connection()
+    # wait_for_internet_connection()
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    IP = '127.'
     try:
-        while '127' in IP:
-            s.connect(('10.255.255.255', 1))
-            IP = s.getsockname()[0]
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
     except Exception as e:
         IP = e
     finally:
         s.close()
+
     return IP
 
 
@@ -36,27 +39,29 @@ def display_on_board(text):
         # Initialize library.
         show.Init()
         show.ClearBlack()
-
         # Create blank image for drawing.
         image1 = Image.new('1', (show.width, show.height), "WHITE")
         draw = ImageDraw.Draw(image1)
         draw.rectangle((0, 0, 127, 31), outline=0)
         draw.text((5, 2), text,
-                  font=ImageFont.truetype('/home/jointload/Desktop/EPIC-Lab-Scripts/Display IP/Font.ttf', 18))
+                  font=ImageFont.truetype('/home/jointload/Desktop/EPIC-Lab-Scripts/Display IP/Font.ttf', 17))
         show.ShowImage(show.getbuffer(image1))
         time.sleep(2)
-
         show.ShowImage(show.getbuffer(image1))
         show.Closebus()
-
     except IOError as e:
         show.Closebus()
         print(e)
-
     except KeyboardInterrupt:
         print("ctrl + c:")
         show.Closebus()
 
 
+def main():
+    IP = get_ip()
+    display_on_board(IP)
+
+
 if __name__ == '__main__':
-    print(get_ip())
+    ip_thread = threading.Thread(target=main, name="main")
+    ip_thread.start()
