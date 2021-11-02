197ba4a2590d8c6cdddef936da182cd358c1ad8c
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-11-02 14:46:12 -0400

    added RIO data processing

--- EMG Data Processing/run_main.m
+++ EMG Data Processing/run_main.m
@@ -1,16 +1,21 @@
 process_data_script = 'process_data.m';
 filter_data_script = 'filter_data.m';
 neuro_muscular_model_script = 'NMM_script2.m';
+rio_data_script = 'rio_data.m';
+
 combined_assistance = 0;
 
 for muscle_number = 2:4 
     run(process_data_script);
     run(filter_data_script);
     run(neuro_muscular_model_script);
+    run(rio_data_script);
     combined_assistance = combined_assistance - assistance;
     if muscle_number == 2
         combined_assistance = combined_assistance + 2*assistance;
     end
     %hold on;
 end
-plot(time,combined_assistance); 
+plot(time,combined_assistance);
+hold on;
+plot(time,biological_torque_estimate)
